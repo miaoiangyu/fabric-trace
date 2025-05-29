edff6b60b700162f48871ec9db0d72c404e1c2b4
miaoiangyu <904261465@qq.com>
2025-05-29 18:01:13 +0800

    index.vue

--- application/web/src/views/login/index.vue
+++ application/web/src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title" style="color: white;">基于区块链的农产品溯源系统</h3>
+        <h3 class="title" style="color: white;">基于区块链的食品安全备忘录系统</h3>
       </div>
       <div v-show="isLoginPage">
         <el-form-item prop="username">
@@ -135,14 +135,14 @@ export default {
         userType: ''
       },
       options: [{
-        value: '种植户',
-        label: '种植户'
+        value: '食品供应商',
+        label: '食品供应商'
       }, {
         value: '工厂',
         label: '工厂'
       }, {
-        value: '运输司机',
-        label: '运输司机'
+        value: '物流公司',
+        label: '物流公司'
       }, {
         value: '商店',
         label: '商店'
